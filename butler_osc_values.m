1c99e31dc3a8efe6b576562a101e22ee2b26854e
Thomas S.  Knutsen <la3pna@gmail.com>
2013-09-25 22:23:08 +0200

    butler oscilator values  added

---
+++ butler_osc_values.m
@@ -0,0 +1,22 @@
+%program for buttler collector coupled oscilators.
+
+%Calculates inductor and capacitor values. 
+%frequency in MHz
+%Capacitance in pF
+%Inductance in µH
+F = 14 ; %frequency in MHz. 
+
+f = F*10^6;
+w = 2*pi*f;
+
+c1calc = 1/(w*159); %capacitor to collector
+C1 = c1calc* 10^12
+
+ c2calc = 1/(w*40); % capacitor to ground 
+ C2 = c2calc * 10^12
+ 
+ l1calc = 100/w; % inductor to supply
+ L1 = l1calc * 10^6
+ 
+ ccpl = 1/(w*320);
+Ccpl = ccpl*10^12
